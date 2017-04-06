4ee96b417529ae04795dfcb8b31626785326d625
benzwjian <zwjian@gmail.com>
2017-04-06 10:33:33 +0800

    add notes

--- src/ecpair.js
+++ src/ecpair.js
@@ -6,12 +6,26 @@ var base58 = require('bs58')
 var ecurve = require('ecurve')
 var ecurveSecp256k1 = ecurve.getCurveByName('secp256k1')
 
+/* Returns EC points
+ * @param {Buffer} payload - The random number
+ */
 var ecpoints = (payload) => ecurveSecp256k1.G.multiply(payload)
 
+/* Returns compressed or uncompressed EC points
+ * @param {boolean} isCompressed - Compressed or uncompressed
+ */
 var compress = (isCompressed) => (ecpoints) => ecpoints.getEncoded(isCompressed).toString('hex')
 
-var publicKey = (isCompressed) => R.compose(R.toUpper, compress(isCompressed), ecpoints, utils.bigify)
+/* Returns EC public key
+ * @param {boolean} isCompressed - Compressed or uncompressed
+ * @param {Hex} payload - The random number
+ */
+var publicKey = (isCompressed) => (payload) => R.compose(R.toUpper, compress(isCompressed), ecpoints, utils.bigify)(payload)
 
+/* Returns compressed or uncompressed WIF
+ * @param {boolean} isCompressed - Compressed or uncompressed
+ * @param {Hex} payload - The EC private key
+ */
 var wifPayload = (isCompressed) => (payload) => {
   var fixify = isCompressed
     ? R.compose(utils.suffixBy([0x01]), utils.prefixBy(VERSION.WIF), utils.bufferify)
@@ -24,6 +38,9 @@ var wifPayload = (isCompressed) => (payload) => {
   return wif(payload)
 }
 
+/* Returns WIF and public key
+ @param {Hex} privateKey - The private key
+ */
 var ecpair = (privateKey) => {
   var wif = wifPayload(false)
   var wifCompressed = wifPayload(true)
