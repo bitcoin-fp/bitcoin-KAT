7137b3400bfd223cd763e65a211ab4eedb0e7755
benzwjian <zwjian@gmail.com>
2017-04-07 17:45:19 +0800

    add notes

--- src/address.js
+++ src/address.js
@@ -3,6 +3,9 @@ var utils = require('./utils')
 var VERSION = require('./const').VERSION
 var R = require('ramda')
 
+/* Returns bitcoin address
+ * {HexString} publicKey -> {String} address
+ */
 var address = R.compose(crypto.base58Check, utils.prefixBy(VERSION.ADDRESS), crypto.dhash, utils.bufferify)
 
 module.exports = {
