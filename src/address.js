1312db0602fcbab48fec54f671684a255ac34e4f
benzwjian <zwjian@gmail.com>
2017-04-12 15:44:41 +0800

    add PKH

--- src/address.js
+++ src/address.js
@@ -3,6 +3,7 @@ var utils = require('./utils')
 var R = require('ramda')
 
 /* Returns bitcoin address
+ * @param {Const.NETWORK} network - The mainnet or testnet
  * {HexString} publicKey -> {String} address
  */
 var address = (network) => {
@@ -10,6 +11,22 @@ var address = (network) => {
   return R.compose(crypto.base58Check, prfixedNetwork, crypto.dhash, utils.bufferify)
 }
 
+/* Returns bitcoin address from public key hash
+ * @param {Const.NETWORK} network - The mainnet or testnet
+ * {HexString} publicKeyHash -> {String} address
+ */
+var addressPKH = (network) => {
+  var prfixedNetwork = utils.prefixBy(network)
+  return R.compose(crypto.base58Check, prfixedNetwork, utils.bufferify)
+}
+
+/* Returns public key hash from address
+ * @param {String} address - The bitcoin address
+ */
+var getPKH = (address) => crypto.dBase58Check(address).toString('hex')
+
 module.exports = {
-  address: address
+  address: address,
+  addressPKH: addressPKH,
+  getPKH: getPKH
 }
--- test/address.js
+++ test/address.js
@@ -2,6 +2,8 @@
 
 var assert = require('assert')
 var address = require('../src/address').address
+var addressPKH = require('../src/address').addressPKH
+var getPKH = require('../src/address').getPKH
 var NETWORK = require('../src/const').NETWORK
 
 describe('Address', function () {
@@ -9,11 +11,23 @@ describe('Address', function () {
     var publicKey = '0202a406624211f2abbdc68da3df929f938c3399dd79fac1b51b0e4ad1d26a47aa'
     var mainAddress = address(NETWORK.MAINNET)
     assert.strictEqual(mainAddress(publicKey), '1PRTTaJesdNovgne6Ehcdu1fpEdX7913CK')
+
+    var publicKeyHash = '751e76e8199196d454941c45d1b3a323f1433bd6'
+    var mainAddressPKH = addressPKH(NETWORK.MAINNET)
+    assert.strictEqual(mainAddressPKH(publicKeyHash), '1BgGZ9tcN4rm9KBzDn7KprQz87SZ26SAMH')
+
+    assert.strictEqual(getPKH('1BgGZ9tcN4rm9KBzDn7KprQz87SZ26SAMH'), publicKeyHash)
   })
 
   it('should be valid bitcoin address on test network', function () {
     var publicKey = '024289801366bcee6172b771cf5a7f13aaecd237a0b9a1ff9d769cabc2e6b70a34'
     var testAddress = address(NETWORK.TESTNET)
     assert.strictEqual(testAddress(publicKey), 'n1TteZiR3NiYojqKAV8fNxtTwsrjM7kVdj')
+
+    var publicKeyHash = '751e76e8199196d454941c45d1b3a323f1433bd6'
+    var mainAddressPKH = addressPKH(NETWORK.TESTNET)
+    assert.strictEqual(mainAddressPKH(publicKeyHash), 'mrCDrCybB6J1vRfbwM5hemdJz73FwDBC8r')
+
+    assert.strictEqual(getPKH('mrCDrCybB6J1vRfbwM5hemdJz73FwDBC8r'), publicKeyHash)
   })
 })
